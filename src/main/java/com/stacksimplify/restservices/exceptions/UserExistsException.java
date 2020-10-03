4425dd3a1e123ba83f319a460ca6d6e326167227
sirishavuppu <sirishavuppu@10.0.0.210>
2020-10-02 21:40:14 -0400

    ResponseStatusException First Commit

---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserExistsException.java
@@ -0,0 +1,14 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class UserExistsException extends Exception{
+
+
+	private static final long serialVersionUID = 1L;
+
+	public UserExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
